993198279ac657dfb3c1549ab6e111b9e623d44e
Lean <basedlainpilled@gmail.com>
2023-05-22 08:23:35 -0400

    use `run.chain` in physics plugin

--- packages/physics/src/plugin.ts
+++ packages/physics/src/plugin.ts
@@ -1,7 +1,6 @@
 import { CoreSchedule, run, WorldBuilder } from "thyseus";
 
 import { createCharacters } from "./systems/characters/createCharacters";
-import { moveCharacters } from "./systems/characters/moveCharacters";
 import { saveCharacterPositions } from "./systems/characters/saveCharacterPositions";
 import { createBoxColliders } from "./systems/colliders/createBoxColliders";
 import { createCapsuleColliders } from "./systems/colliders/createCapsuleColliders";
@@ -15,32 +14,25 @@ import { rotateRigidBodies } from "./systems/rigidbodies/rotateRigidBodies";
 import { saveRigidBodyPositions } from "./systems/rigidbodies/saveRigidBodyPositions";
 import { stepWorld } from "./systems/stepWorld";
 
-const createRigidBodies = [
-  createDynamicBodies,
-  createKinematicBodies,
-  createStaticBodies,
-];
-
-const createColliders = [
-  createBoxColliders,
-  createCapsuleColliders,
-  createCylinderColliders,
-  createSphereColliders,
-];
-
-const transformRigidBodies = [moveRigidBodies, rotateRigidBodies];
-
-const savePositions = [saveRigidBodyPositions, saveCharacterPositions];
-
 export function physicsPlugin(builder: WorldBuilder) {
   builder.addSystemsToSchedule(
     CoreSchedule.FixedUpdate,
-    ...createRigidBodies,
-    ...createColliders.map((fn) => run(fn).after(...createRigidBodies)),
-    ...transformRigidBodies.map((fn) => run(fn).after(...createColliders)),
-    createCharacters,
-    run(moveCharacters).after(createCharacters, ...transformRigidBodies),
-    run(stepWorld).after(moveCharacters),
-    ...savePositions.map((fn) => run(fn).after(stepWorld))
+    ...run.chain(
+      [
+        createCharacters,
+        createDynamicBodies,
+        createKinematicBodies,
+        createStaticBodies,
+      ],
+      [
+        createBoxColliders,
+        createCapsuleColliders,
+        createCylinderColliders,
+        createSphereColliders,
+      ],
+      [moveRigidBodies, rotateRigidBodies],
+      stepWorld,
+      [saveCharacterPositions, saveRigidBodyPositions]
+    )
   );
 }
