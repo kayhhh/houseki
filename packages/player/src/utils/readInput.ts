74c124b58ba583685ce22001d6aca2f17bc577de
Lean <basedlainpilled@gmail.com>
2023-05-24 11:19:53 -0400

    fix input normalize math

--- packages/player/src/utils/readInput.ts
+++ packages/player/src/utils/readInput.ts
@@ -28,5 +28,9 @@ export function readInput(inputStruct: InputStruct): Input {
 
 function normalize(x: number, y: number) {
   const length = Math.sqrt(x * x + y * y);
-  return { x: x / length, y: y / length };
+
+  const normalX = x === 0 ? 0 : x / length;
+  const normalY = y === 0 ? 0 : y / length;
+
+  return { x: normalX, y: normalY };
 }
