8e9f7e8fb5caaa443cec654c73e86343d183b029
Lean <basedlainpilled@gmail.com>
2023-05-29 12:40:39 -0400

    remove register components from scene plugin

--- packages/scene/src/plugin.ts
+++ packages/scene/src/plugin.ts
@@ -5,12 +5,7 @@ import {
   keyframeTrackCleanup,
   textureCleanup,
 } from "./cleanup";
-import { AnimationClip, AnimationMixer, KeyframeTrack } from "./components";
 
 export function scenePlugin(builder: WorldBuilder) {
-  builder
-    .registerComponent(AnimationMixer)
-    .registerComponent(AnimationClip)
-    .registerComponent(KeyframeTrack)
-    .addSystems(geometryCleanup, textureCleanup, keyframeTrackCleanup);
+  builder.addSystems(geometryCleanup, textureCleanup, keyframeTrackCleanup);
 }
