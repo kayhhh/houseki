01eb502e72960aa249adfe699bd522bbb49c1a99
Lean <basedlainpilled@gmail.com>
2023-05-31 19:54:20 -0400

    add animation smoothing

--- packages/player/src/systems/animatePlayer.ts
+++ packages/player/src/systems/animatePlayer.ts
@@ -1,63 +1,89 @@
-import { InputStruct } from "@lattice-engine/input";
+import { MainLoopTime } from "@lattice-engine/core";
 import { CharacterController, Velocity } from "@lattice-engine/physics";
 import { Parent } from "@lattice-engine/scene";
 import { VrmAnimation } from "@lattice-engine/vrm";
-import { Entity, Mut, Query, Res, With } from "thyseus";
+import { Entity, Mut, Query, Res } from "thyseus";
 
 import { PlayerAvatar, PlayerBody } from "../components";
-import { readInput } from "../utils/readInput";
+
+const WALK_SPEED = 5;
+const SPRINT_SPEED = 5;
+const JUMP_SPEED = 5;
 
 export function animatePlayer(
-  inputStruct: Res<InputStruct>,
+  time: Res<MainLoopTime>,
   avatars: Query<[Entity, Parent, PlayerAvatar]>,
   animations: Query<[Entity, Mut<VrmAnimation>]>,
-  bodies: Query<[Entity, CharacterController, Velocity], With<PlayerBody>>
+  bodies: Query<[Entity, PlayerBody, CharacterController, Velocity]>
 ) {
-  const input = readInput(inputStruct);
-
-  const walkWeight = Math.abs(input.x) + Math.abs(input.y);
-
   for (const [entity, parent, avatar] of avatars) {
-    for (const [bodyEntity, character, velocity] of bodies) {
+    for (const [bodyEntity, player, character, velocity] of bodies) {
       if (bodyEntity.id !== parent.id) continue;
+
+      const moveLength = Math.sqrt(
+        Math.abs(velocity.x) ** 2 + Math.abs(velocity.z) ** 2
+      );
+      const isSprinting = moveLength > player.speed * 1.05;
+      const isWalking = moveLength / player.speed > 0.25 && !isSprinting;
+
+      let totalWeight = 0;
 
       for (const [animationEntity, animation] of animations) {
         if (animation.vrmId !== entity.id) continue;
-
-        const jumpWeight = character.isGrounded ? 0 : 1;
+        if (animationEntity.id === avatar.idleAnimationId) continue;
 
         switch (animationEntity.id) {
-          case avatar.idleAnimationId: {
-            animation.weight = 1 - walkWeight - jumpWeight;
-            animation.play = animation.weight > 0;
-            break;
-          }
-
           case avatar.walkAnimationId: {
-            animation.weight = jumpWeight === 0 ? walkWeight : 0;
-            animation.play = animation.weight > 0;
+            const change =
+              isWalking && character.isGrounded ? WALK_SPEED : -WALK_SPEED;
+            animation.weight += time.delta * change;
             break;
           }
 
           case avatar.sprintAnimationId: {
+            const change =
+              isSprinting && character.isGrounded
+                ? SPRINT_SPEED
+                : -SPRINT_SPEED;
+            animation.weight = clamp(animation.weight + time.delta * change);
             break;
           }
 
           case avatar.jumpAnimationId: {
-            animation.weight = jumpWeight;
-            animation.play = animation.weight > 0;
+            const change = character.isGrounded ? -JUMP_SPEED : JUMP_SPEED;
+            animation.weight += time.delta * change;
             break;
           }
 
           case avatar.leftWalkAnimationId: {
+            animation.weight = 0;
             break;
           }
 
           case avatar.rightWalkAnimationId: {
+            animation.weight = 0;
             break;
           }
         }
+
+        animation.weight = clamp(animation.weight);
+        animation.play = animation.weight > 0;
+
+        totalWeight += animation.weight;
+      }
+
+      // Idle animation
+      for (const [animationEntity, animation] of animations) {
+        if (animation.vrmId !== entity.id) continue;
+        if (animationEntity.id !== avatar.idleAnimationId) continue;
+
+        animation.weight = clamp(1 - totalWeight);
+        animation.play = animation.weight > 0;
       }
     }
   }
+}
+
+function clamp(value: number, min = 0, max = 1) {
+  return Math.max(Math.min(value, max), min);
 }
