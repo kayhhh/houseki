ac7bac94c3062bff3d50828ec9801fa164811224
Kayh <basedlainpilled@gmail.com>
2023-09-11 13:46:54 -0400

    remove lights from gltf example

--- examples/src/demos/gltf/systems.ts
+++ examples/src/demos/gltf/systems.ts
@@ -1,10 +1,9 @@
-import { CoreStore, Warehouse } from "houseki/core";
+import { CoreStore } from "houseki/core";
 import { Gltf } from "houseki/gltf";
 import { N8AOPass } from "houseki/postprocessing";
 import { SceneStruct } from "houseki/scene";
 import { Commands, Mut, Query, Res } from "thyseus";
 
-import { createLights } from "../../utils/createLights";
 import { createOrbitControls } from "../../utils/createOrbitControls";
 import { createScene } from "../../utils/createScene";
 
@@ -19,20 +18,15 @@ export function initScene(
 ) {
   createOrbitControls(commands, sceneStruct);
   const { rootId, sceneId } = createScene(commands, coreStore, sceneStruct);
-  createLights(commands, sceneId);
 
   commands.getById(sceneId).addType(N8AOPass);
-
   commands.getById(rootId).addType(Gltf);
 }
 
 /**
  * System to update the glTF uri.
  */
-export function loadGltf(
-  warehouse: Res<Mut<Warehouse>>,
-  entities: Query<Mut<Gltf>>
-) {
+export function loadGltf(entities: Query<Mut<Gltf>>) {
   for (const gltf of entities) {
     gltf.uri = selectedModel.uri;
   }
